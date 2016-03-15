886332213a65a8eafa25ae3a7cac0585949fe539
roosness <roos95@live.nl>
2016-03-15 12:53:11 +0100

    header omg smaller

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,33 @@
-$(document).ready(function(){
-    $('.product').first().addClass('product-active');
-    $(".product-indicator[data-uuid='"+$('.product').first().data('uuid')+"']").addClass('product-indicator-active');
+(function () {
+    "use strict";
+    
+    var product = document.querySelector('.product');
+    
+    function change(e) {
+        var cur = e.currentTarget;
+        document.querySelector('.product-indicator-active').classList.remove('product-indicator-active');
+        cur.classList.add('product-indicator-active');
+        document.querySelector('.product-active').classList.remove('product-active');
+        document.querySelector('.product[data-uuid="' + cur.getAttribute('data-uuid') + '"]').classList.add('product-active');
+    }
+    
+    function init() {
+        var productAttr = product.getAttribute('data-uuid'),
+            indicator = document.querySelector('.product-indicator[data-uuid="' + productAttr + '"]'),
+            indicators = document.getElementsByClassName('product-indicator');
 
-    $('.product-indicator').on('click', function(e){
-        var id = $(e.currentTarget).data('uuid');
-        $('.product-indicator-active').removeClass('product-indicator-active');
-        $(e.currentTarget).addClass('product-indicator-active');
-        $('.product.product-active').removeClass('product-active');
-        $(".product[data-uuid='"+id+"']").addClass('product-active');
-    });
-});
+        product.classList.add('product-active');
+        indicator.classList.add('product-indicator-active');
+
+        [].forEach.call(indicators, function (elem) {
+            elem.addEventListener('click', change, false);
+        });
+    }
+    
+    if (document.querySelector && window.addEventListener) {
+        if (product) {
+            init();
+        }
+    }
+              
+}());
